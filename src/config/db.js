c54d31368abbba7df1501fa4bdbee73f26118a67
Lalit Singh <132752354+ltlalit15@users.noreply.github.com>
2025-07-11 18:41:17 +0530

    Update db.js

--- src/config/db.js
+++ src/config/db.js
@@ -5,10 +5,11 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const pool = mysql.createPool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
+  host: 'yamabiko.proxy.rlwy.net',
+  user: 'root',
+  password: 'fgUqpTopJxyfUJpLehoKYVoTVImDmZso',
+  database: 'railway',
+  port: 19486,
   waitForConnections: true,
   connectionLimit: 10,
 });
