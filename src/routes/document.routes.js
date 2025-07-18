4ecfa0782ed59f96d17467e2b3ef4290c3d2635f
Lalit Singh <132752354+ltlalit15@users.noreply.github.com>
2025-07-18 11:32:50 +0530

    Update document.routes.js

--- src/routes/document.routes.js
+++ src/routes/document.routes.js
@@ -1,10 +1,12 @@
 import express from 'express';
 
 import upload from '../middleware/upload.middleware.js';
-import { uploadDocument } from '../controllers/document.controller.js';
+import { getDocuments, uploadDocument } from '../controllers/document.controller.js';
 
 const router = express.Router();
 
 router.post('/upload', upload.single('document_file'), uploadDocument);
+
+router.get('/', getDocuments);
 
 export default router;
