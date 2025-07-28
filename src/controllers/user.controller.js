0ce03e202b40160985640f73b6180d64e7768871
Lalit Singh <132752354+ltlalit15@users.noreply.github.com>
2025-07-28 13:48:31 +0530

    Update user.controller.js

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -145,7 +145,7 @@ export const loginUser = async (req, res) => {
 
   const token = jwt.sign(
     { user_id: user.user_id, role: user.role },
-    'a3b5c1d9e8f71234567890abcdef1234567890abcdefabcdef1234567890yyweyw',
+    'supersecretkey',
     { expiresIn: "1d" }
   );
   res.json({ token, user, role });
